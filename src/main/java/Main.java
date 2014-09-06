b57cbfa3429b8c99cbd8e202fb23c9f10f5b8021
tomas <tomasabril@users.noreply.github.com>
2014-09-06 18:01:47 -0300

    inicio

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * IF62C - Fundamentos de Programação 2
+ * 
+ * por Tomás Abril
+ */
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Olá, Java!");
+    }
+}
